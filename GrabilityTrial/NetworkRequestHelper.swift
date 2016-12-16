296b4219ac710f7d6021908234d40ad9dccfa630
jveleze <jeffrsk8@hotmail.com>
2016-12-16 14:06:23 -0500

    - Se incluye el módulo que le permite a la app funcionar offline

--- GrabilityTrial/NetworkRequestHelper.swift
+++ GrabilityTrial/NetworkRequestHelper.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import Alamofire
+import ObjectMapper
 import AlamofireObjectMapper
 
 protocol DataSyncronizedProtocol {
@@ -18,17 +19,37 @@ class NetworkRequestHelper: NSObject {
     
     var delegate : DataSyncronizedProtocol?
     let urlServices = "https://itunes.apple.com/us/rss/topfreeapplications/limit=20/json"
+    var localDataHandler = LocalDataHandler()
     
     func syncData() {
         
         Alamofire.request(urlServices).responseObject { (response: DataResponse<JsonMap>) in
             
+            switch response.result {
+            case .success:
+                if response.result.value != nil {
+                    JsonInfoDTO.sharedJsonInfoDTO.jsonMap = response.result.value
+                    self.localDataHandler.saveFile(jsonToSave: response.result.value!)
+                    self.delegate?.dataSyncronized()
+                } else {
+                    print("Request Error")
+                }
+            case .failure(let error):
+                print(error)
+                print("Data loaded from local")
+                let jsonString:String = (self.localDataHandler.readFile())
+                JsonInfoDTO.sharedJsonInfoDTO.jsonMap = Mapper<JsonMap>().map(JSONString: jsonString)
+                self.delegate?.dataSyncronized()
+            }
+            
+            /*
             if response.result.value != nil {
                 JsonInfoDTO.sharedJsonInfoDTO.jsonMap = response.result.value
-                    self.delegate?.dataSyncronized()
+                self.dataSaver.saveFile(json: response.result.value!)
+                self.delegate?.dataSyncronized()
             } else {
                 print("Request Error")
-            }
+            }*/
         }
     }
     
---
+++ LocalDataHandler.swift
@@ -0,0 +1,41 @@
+//
+//  LocalDataHandler.swift
+//  GrabilityTrial
+//
+//  Created by Jefferson Vélez Escobar on 16/12/16.
+//  Copyright © 2016 Jefferson Vélez Escobar. All rights reserved.
+//
+
+import UIKit
+
+class LocalDataHandler: NSObject {
+    
+    var extendedPath = "/Library/Caches/ServiceData.json"
+    
+    override init(){}
+    
+    func saveFile(jsonToSave:JsonMap)
+    {
+        let jsonMap = jsonToSave.toJSONString()
+        let filePath = NSHomeDirectory() + "/Library/Caches/jsonData.json"
+        do {
+            _ = try jsonMap?.write(toFile: filePath, atomically: true, encoding: String.Encoding.utf8)
+        } catch let error as NSError {
+            print(error.description)
+        }
+    }
+    
+    func readFile() -> String
+    {
+        var readString = ""
+        let filePath = NSHomeDirectory() + "/Library/Caches/jsonData.json"
+        do {
+            readString = try NSString(contentsOfFile: filePath, encoding: String.Encoding.utf8.rawValue) as String
+        } catch let error as NSError {
+            print(error.description)
+            print("You need to connect to internet at least once")
+        }
+        return readString
+    }
+    
+}
