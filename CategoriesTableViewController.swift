2a74fb0385221713cbb27349de7f72531a5e6fb5
jveleze <jeffrsk8@hotmail.com>
2016-12-20 08:40:09 -0500

    - Se ajustan últimos detalles gráficos

--- CategoriesTableViewController.swift
+++ CategoriesTableViewController.swift
@@ -90,7 +90,6 @@ class CategoriesTableViewController: UITableViewController, DataSyncronizedProto
         let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath) as! CategoriesTableViewCell
         let category = self.categories[indexPath.row]
         cell.categoryName.text = category
-        cell.backgroundColor = UIColor.black
 
         return cell
     }
