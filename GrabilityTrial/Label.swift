41277b55ed8697123febb1686a96dbc3a2cfd155
jveleze <jeffrsk8@hotmail.com>
2016-12-20 08:56:09 -0500

    upload missing cocoapods files

---
+++ GrabilityTrial/Label.swift
@@ -0,0 +1,22 @@
+//
+//  LastKeyString.swift
+//  GrabilityTrial
+//
+//  Created by Jefferson Vélez Escobar on 14/12/16.
+//  Copyright © 2016 Jefferson Vélez Escobar. All rights reserved.
+//
+
+import Foundation
+import ObjectMapper
+
+class Label: Mappable{
+    
+    var label: String?
+    
+    required init?(map: Map) {
+    }
+    
+    func mapping(map: Map) {
+        label <- map["label"]
+    }
+}
